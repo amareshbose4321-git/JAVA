e183aad129ff0bced14d94db99c7acf7da5f1c2f
Student amaresh <amareshbose4321@gmail.com>
2026-01-20 19:34:36 +0530

    Add something

---
+++ Loops.java
@@ -0,0 +1,79 @@
+public class Loops {
+    public static void main(String[] args) {
+        
+        //LOOPS
+        
+        //for loop
+
+        //Print number 1-100
+//        for(int i = 1 ; i<=100 ; i++){
+//            System.out.println(i);
+//        }
+
+        //Print number 100-1
+//        for(int i = 100 ; i>=1 ; i--){
+//            System.out.println(i);
+//        }
+
+        //while loop
+//        int j = 100;
+//        while(j>=1){
+//            System.out.println(j);
+//            j--;
+//        }
+
+        //Do-while loop
+//        int k = 100;
+//        do{
+//            System.out.println(k);
+//            k--;
+//        } while (k>=1);
+
+
+// take positive numbers from user and print that number until the number is negative.
+
+        //Using do-while loop
+//        Scanner sc = new Scanner(System.in);
+//        int number = 0;
+//        do {
+//            System.out.println("Enter a number: ");
+//            number = sc.nextInt();
+//            System.out.println("Your number is: " + number);
+//        } while (number >= 0);
+//        System.out.println("THE END");
+
+
+        //Using for loop
+//        for(;;){
+//            Scanner sc = new Scanner(System.in);
+//            System.out.println("Enter a number: ");
+//            int number = sc.nextInt();
+//
+//            if(number < 0){
+//                System.out.println("THE END");
+//            }
+//            else {
+//
+//                System.out.println("Your number is: " + number);
+//            }
+//        }
+
+
+        //Using while loop
+//        int number = 0;
+//        while(number >=  0){
+//            Scanner sc = new Scanner(System.in);
+//            System.out.println("Enter a number: ");
+//            number = sc.nextInt();
+//            if(number < 0){
+//                System.out.println("THE END");
+//            }
+//            else {
+//
+//                System.out.println("Your number is: " + number);
+//            }
+//        }
+
+
+    }
+}
