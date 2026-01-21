2da384668c651fb4522d31861e052cedf7662866
Student amaresh <amareshbose4321@gmail.com>
2026-01-21 11:44:10 +0530

    Update

--- Loops.java
+++ Loops.java
@@ -5,75 +5,82 @@ public class Loops {
 
         //for loop
 
-        //Print number 1-100
-//        for(int i = 1 ; i<=100 ; i++){
+        //Print number 1-5
+//        for(int i = 1 ; i<=5 ; i++){
 //            System.out.println(i);
 //        }
-        //Ex:
+       
 
-        //Print number 100-1
-//        for(int i = 100 ; i>=1 ; i--){
+        //Print number 5-1
+//        for(int i = 5 ; i>=1 ; i--){
 //            System.out.println(i);
 //        }
 
         //while loop
-//        int j = 100;
+//        int j = 5;
 //        while(j>=1){
 //            System.out.println(j);
 //            j--;
 //        }
 
         //Do-while loop
-//        int k = 100;
+//        int k = 5;
 //        do{
 //            System.out.println(k);
 //            k--;
 //        } while (k>=1);
 
+// Difference betwwen while and do while:
+// In while we 1st check the condition and the execute the loop.
+// In do-while 1st atleast one time operation perform and then check the condition to execute loop. 
 
 // take positive numbers from user and print that number until the number is negative.
 
-        //Using do-while loop
-//        Scanner sc = new Scanner(System.in);
-//        int number = 0;
-//        do {
-//            System.out.println("Enter a number: ");
-//            number = sc.nextInt();
-//            System.out.println("Your number is: " + number);
-//        } while (number >= 0);
-//        System.out.println("THE END");
-
-
-        //Using for loop
-//        for(;;){
+        //Using for lopp :
+        
+//        System.out.println("----START----");
+//        for(;;){ // infinite loop
 //            Scanner sc = new Scanner(System.in);
 //            System.out.println("Enter a number: ");
 //            int number = sc.nextInt();
 //
 //            if(number < 0){
-//                System.out.println("THE END");
+//                System.out.println("----THE END----");
+//                break; //stop the loop
 //            }
 //            else {
-//
 //                System.out.println("Your number is: " + number);
 //            }
 //        }
 
+        
+          //Using while loop :
 
-        //Using while loop
-//        int number = 0;
-//        while(number >=  0){
+//        int num = 0;
+//        System.out.println("----START----");
+//        while (num >= 0){
 //            Scanner sc = new Scanner(System.in);
 //            System.out.println("Enter a number: ");
-//            number = sc.nextInt();
-//            if(number < 0){
-//                System.out.println("THE END");
-//            }
-//            else {
-//
-//                System.out.println("Your number is: " + number);
+//            num = sc.nextInt();
+//            if(num < 0){
+//                System.out.println("----THE END----");
+//            } else {
+//                System.out.println("Your number is: "+num);
 //            }
 //        }
+        
+        
+        //Using do-while loop :
+
+//        Scanner sc = new Scanner(System.in);
+//        int num = 0;
+//        System.out.println("----START----");
+//        do {
+//            System.out.println("Enter a number: ");
+//            num = sc.nextInt();
+//            System.out.println("Your number is: "+num);
+//        } while (num >= 0);
+//        System.out.println("----THE END----");
 
 
     }
