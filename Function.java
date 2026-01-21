1b7079cf5d10c8b243a16db41865522bbf5fe441
Student amaresh <amareshbose4321@gmail.com>
2026-01-21 12:00:58 +0530

    Add function

---
+++ Function.java
@@ -0,0 +1,34 @@
+// What is a Function in Java?
+
+// In Java, a function is called a method.
+// A method is a block of code that performs a specific task and runs only when it is called.
+
+
+// 👉 Methods help to:
+
+// Avoid code repetition
+// Make code reusable
+// Improve readability and maintenance
+
+
+// 🔹 Syntax of a Method (Function)
+
+// returnType methodName(parameters) {
+//     // code
+//     return value; // optional (if void the no return, if int/float/double..etc then return)
+// }
+
+// 🔹 What is Function Calling?
+
+// Function calling means invoking (using) a method so that the code inside it executes.
+
+
+// Example:
+
+// methodName();      // calling a method
+
+
+
+public class Function {
+    
+}
